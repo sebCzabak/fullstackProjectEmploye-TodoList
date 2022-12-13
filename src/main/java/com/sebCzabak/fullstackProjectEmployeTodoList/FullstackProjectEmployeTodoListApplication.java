e54b3cab541853e021a41434fa3d789a10bb98fa
sebCzabak <sczabak@gmail.com>
2022-12-13 10:44:04 +0100

    Initial commit

---
+++ src/main/java/com/sebCzabak/fullstackProjectEmployeTodoList/FullstackProjectEmployeTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.sebCzabak.fullstackProjectEmployeTodoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackProjectEmployeTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackProjectEmployeTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sebCzabak/fullstackProjectEmployeTodoList/FullstackProjectEmployeTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebCzabak.fullstackProjectEmployeTodoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackProjectEmployeTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
